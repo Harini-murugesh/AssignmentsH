061bb0475e2ef9f42b11bf1e1befd3e701d4b257
Harini-murugesh <143285013+Harini-murugesh@users.noreply.github.com>
2025-03-18 19:51:35 +0530

    task1.sql

--- task1.sql
+++ task1.sql
@@ -8,49 +8,35 @@ CREATE TABLE Venue (
     address VARCHAR(255) NOT NULL
 );
 
-
 -- Create Event Table
 CREATE TABLE Event (
-    event_id INT IDENTITY(1,1) PRIMARY KEY,
+    event_id INT PRIMARY KEY,
     event_name VARCHAR(100) NOT NULL,
     event_date DATE NOT NULL,
     event_time TIME NOT NULL,
-    venue_id INT NOT NULL,
+    venue_id INT NOT NULL REFERENCES Venue(venue_id),
     total_seats INT NOT NULL,
     available_seats INT NOT NULL,
     ticket_price DECIMAL(10,2) NOT NULL,
     event_type VARCHAR(50) NOT NULL CHECK (event_type IN ('Movie', 'Sports', 'Concert')),
-    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE CASCADE
+    booking_id INT REFERENCES Booking(booking_id) 
 );
-DROP TABLE IF EXISTS Event;
 
-CREATE TABLE Event (
-    event_id INT IDENTITY(1,1) PRIMARY KEY,
-    event_name VARCHAR(100) NOT NULL,
-    event_date DATE NOT NULL,
-    event_time TIME NOT NULL,
-    venue_id INT NOT NULL,
-    total_seats INT NOT NULL,
-    available_seats INT NOT NULL,
-    ticket_price DECIMAL(10,2) NOT NULL,
-    event_type VARCHAR(50) NOT NULL CHECK (event_type IN ('Movie', 'Sports', 'Concert')),
-    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE CASCADE
-);
 -- Create Customer Table
 CREATE TABLE Customer (
-    customer_id INT IDENTITY(1,1) PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL
+    customer_id INT PRIMARY KEY,
+    customer_name NVARCHAR(30) NOT NULL,
+    email nVARCHAR(30) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+booking_id INT REFERENCES Booking(booking_id)
 );
+
 -- Create Booking Table
 CREATE TABLE Booking (
-    booking_id INT IDENTITY(1,1) PRIMARY KEY,
-    customer_id INT NOT NULL,
-    event_id INT NOT NULL,
+    booking_id INT PRIMARY KEY,
+    customer_id INT REFERENCES Customer(customer_id),
+    event_id INT REFERENCES Event(event_id),
     num_tickets INT NOT NULL,
     total_cost DECIMAL(10,2) NOT NULL,
-    booking_date DATETIME NOT NULL DEFAULT GETDATE(),
-   FOREIGN KEY (customer_id) REFERENCES Customer(customer_id) ON DELETE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES Event(event_id) ON DELETE CASCADE
+    booking_date DATETIME NOT NULL DEFAULT GETDATE()
 );
